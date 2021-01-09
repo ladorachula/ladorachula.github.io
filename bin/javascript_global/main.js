de2bd66a8935b599b475f3c6d7a9da8023941656
Floreroo <locodediscord@gmail.com>
2021-01-09 22:13:29 +0100

    :3

--- bin/javascript_global/main.js
+++ bin/javascript_global/main.js
@@ -1,2 +1,21 @@
-var hiddenX=false;function windowOpen(){let myWindow=window.open("https://discord.com/api/oauth2/authorize?client_id=752529751943544902&permissions=8&scope=bot","affiliateagreement","width=1000,height=1000");$(window).focus(function(){if(hiddenX)return;window.location.reload();myWindow.close(); hiddenX=true;});};function sendReport(url){var request=new Request(url);fetch(request,{mode:'no-cors'})
-.then(function(response){}).catch(function(error){console.log('Request failed',error);});}
+var hiddenX = false;
+
+function windowOpen() {
+  let myWindow = window.open("https://discord.com/api/oauth2/authorize?client_id=752529751943544902&permissions=8&scope=bot", "affiliateagreement", "width=1000,height=1000");
+  $(window).focus(function() {
+    if (hiddenX) return;
+    window.location.reload();
+    myWindow.close();
+    hiddenX = true;
+  });
+};
+
+function sendReport(url) {
+  var request = new Request(url);
+  fetch(request, {
+      mode: 'no-cors'
+    })
+    .then(function(response) {}).catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
