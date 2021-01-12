3cc0b8de9f4ae2a33ee2be99677e8babc7a954d1
Floreroo <locodediscord@gmail.com>
2021-01-12 23:55:16 +0100

    :3

--- index.js
+++ index.js
@@ -7,25 +7,19 @@ app.engine("html", require('ejs').renderFile);
 app.set("view engine", "ejs");
 
 app.get("/es/", (req, res) => {
- res.render(path.join(__dirname + "/bin/views_ooc/es.html"))
+ res.render("https://ladorachula.github.io/bin/views_ooc/es.html")
 });
 
 app.get("/en/", (req, res) => {
- res.render(path.join(__dirname + "/bin/views_ooc/en.html"));
+ res.render("https://ladorachula.github.io/bin/views_ooc/en.html");
 });
 
 app.get("/github", (req, res) => {
- res.render(path.join(__dirname + "/bin/views_ooc/github.html"));
+ res.render("https://ladorachula.github.io/bin/views_ooc/github.html");
 });
 
 app.get("/discord", (req, res) => {
- res.render(path.join(__dirname + "/bin/views_ooc/discord.html"));
+ res.render("https://ladorachula.github.io/bin/views_ooc/discord.html");
 });
-
-const port = 9999;
-
-app.listen(port, function(){
- console.log("Culo de gótica culona en dirección "+port)
-})
 
 module.exports = app;
