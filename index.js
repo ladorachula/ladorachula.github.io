34af7563b9662ebc7b20454366ae216346e8cbc2
Floreroo <locodediscord@gmail.com>
2021-01-12 23:11:51 +0100

    :3

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const router = express.Router();
 
 app.engine("html", require('ejs').renderFile);
-app.use("/", require("./bin/routes_ooc/index"));
 app.set("view engine", "ejs");
+
+router.get("/es/", (res, req) => {
+ res.render("https://ladorachula.github.io/views_ooc/es.html");
+});
+
+router.get("/en/", (res, req) => {
+ res.render("https://ladorachula.github.io/views_ooc/en.html");
+});
+
+router.get("/github", (res, req) => {
+ res.render("https://ladorachula.github.io/views_ooc/github.html");
+});
+
+router.get("/discord", (res, req) => {
+ res.render("https://ladorachula.github.io/views_ooc/discord.html");
+});
+
+module.exports = router;
