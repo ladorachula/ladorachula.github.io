e048f8769f3b80be1591517409c0b9a5ff327451
Floreroo <locodediscord@gmail.com>
2021-01-12 23:47:15 +0100

    :3

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const router = express.Router();
 
 app.engine("html", require('ejs').renderFile);
 app.set("view engine", "ejs");
 
-app.get("/es/", (res, req) => {
- res.render("./bin/views_ooc/es.html");
+app.get("/es/", (req, res) => {
+ res.render(path.join(__dirname + "/bin/views_ooc/es.html"))
 });
 
-app.get("/en/", (res, req) => {
- res.render("./bin/views_ooc/en.html");
+app.get("/en/", (req, res) => {
+ res.render(path.join(__dirname + "/bin/views_ooc/en.html"));
 });
 
-app.get("/github", (res, req) => {
- res.render("./bin/views_ooc/github.html");
+app.get("/github", (req, res) => {
+ res.render(path.join(__dirname + "/bin/views_ooc/github.html"));
 });
 
-app.get("/discord", (res, req) => {
- res.render(".7bin/views_ooc/discord.html");
+app.get("/discord", (req, res) => {
+ res.render(path.join(__dirname + "/bin/views_ooc/discord.html"));
 });
+
+const port = 9999;
+
+app.listen(port, function(){
+ console.log("Culo de gótica culona en dirección "+port)
+})
 
 module.exports = app;
