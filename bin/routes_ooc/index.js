660e76e3f89e6b69138a7184aad52b6ca08a95c1
Floreroo <locodediscord@gmail.com>
2021-01-12 21:16:54 +0100

    :3

--- bin/routes_ooc/index.js
+++ bin/routes_ooc/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+router.get("/es/", (res, req) => {
+ res.render("https://ladorachula.github.io/views_ooc/es.html");
+});
+
+router.get("/en/", (res, req) => {
+ res.render("https://ladorachula.github.io/views_ooc/en.html");
+});
+
+router.get("/github", (res, req) => {
+ res.render("https://ladorachula.github.io/views_ooc/github.html");
+});
+
+router.get("/discord", (res, req) => {
+ res.render("https://ladorachula.github.io/views_ooc/discord.html");
+});
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-/* const express = require("express");
+const express = require("express");
 const app = express();
 const path = require("path");
 
 app.engine("html", require('ejs').renderFile);
-app.use(express.static(path.join(__dirname, "/.bin/public")));
-app.set("views", path.join(__dirname, "/.bin/views"));
+app.use(express.static("https://ladorachula.github.io/bin/")));
+app.set("views","https://ladorachula.github.io/bin/views_ooc"));
 app.set("view engine", "ejs");
-app.set("port", 3000);
-app.get('/', (req, res) => {
-	res.render(path.join(__dirname + '/index.ejs'));
-});
-app.get('*', function(req, res) {
-  res.send('Error 404!');
-});
-app.listen('3000', function () {
-  console.log('Ready, port 3030');
-})
- */
