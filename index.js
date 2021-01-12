d15f92c333270d5ab5582d32284590eb9f4bfbbc
Floreroo <locodediscord@gmail.com>
2021-01-12 21:20:59 +0100

    :3

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ const app = express();
 const path = require("path");
 
 app.engine("html", require('ejs').renderFile);
-app.use(express.static("https://ladorachula.github.io/bin/")));
-app.set("views","https://ladorachula.github.io/bin/views_ooc"));
+app.use("/", require("./bin/routes_ooc/index"));
 app.set("view engine", "ejs");
