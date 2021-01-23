e8d968397fbb999c1ee73ea4d09041a6b69f219e
ladorachula <76827122+ladorachula@users.noreply.github.com>
2021-01-23 23:53:22 +0100

    :3

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const router = express.Router();
 
 app.engine("html", require('ejs').renderFile);
 app.set("view engine", "ejs");
 
-app.get("/es/", (req, res) => {
- res.sendFile("https://ladorachula.github.io/bin/views_ooc/es.html")
-});
+app
+ .get("/es/", function(req, res) => {
+ res.reder("https://ladorachula.github.io/bin/views_ooc/es.html")
+ })
+ .post(function(req, res) => {
+   console.log("Ruta /es/ posteada.")
+ })
 
-app.get("/en/", (req, res) => {
+
+app
+ .get("/en/", function(req, res) => {
  res.render("https://ladorachula.github.io/bin/views_ooc/en.html");
-});
+ })
+ .post(function(req, res) => {
+   console.log("Ruta /en/ posteada.")
+ })
 
-app.get("/github", (req, res) => {
+app
+ .get("/github/", function(req, res) => {
  res.render("https://ladorachula.github.io/bin/views_ooc/github.html");
-});
+ })
+ .post(function(req, res) => {
+   console.log("Ruta /github/ posteada.")
+ })
 
-app.get("/discord", (req, res) => {
+app
+ .get("/discord/", function(req, res) => {
  res.render("https://ladorachula.github.io/bin/views_ooc/discord.html");
-});
+ })
+ .post(function(req, res) => {
+   console.log("Ruta /discord/ posteada.")
+ })
 
 module.exports = app;
