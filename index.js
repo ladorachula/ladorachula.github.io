9197a47a3687c9b883572b5001b6acb89d152fb2
ladorachula <76827122+ladorachula@users.noreply.github.com>
2021-01-24 00:20:08 +0100

    Send File Update

--- index.js
+++ index.js
@@ -3,30 +3,25 @@ const router = express();
 const path = require("path");
 var router = express.Router();
 
-router.use(function timeLog (req, res, next) {
-  console.log('Time: ', Date.now())
-  next()
-})
-
 router
  .get("/es/", function(req, res) => {
- res.reder("https://ladorachula.github.io/bin/views_ooc/es.html")
+ res.sendFile("https://ladorachula.github.io/bin/views_ooc/es.html")
  })
 
 
 router
  .get("/en/", function(req, res) => {
- res.render("https://ladorachula.github.io/bin/views_ooc/en.html");
+ res.sendFile("https://ladorachula.github.io/bin/views_ooc/en.html");
  })
 
-router
+router 
  .get("/github/", function(req, res) => {
- res.render("https://ladorachula.github.io/bin/views_ooc/github.html");
+ res.sendFile("https://ladorachula.github.io/bin/views_ooc/github.html");
  })
 
 router
  .get("/discord/", function(req, res) => {
- res.render("https://ladorachula.github.io/bin/views_ooc/discord.html");
+ res.sendFile("https://ladorachula.github.io/bin/views_ooc/discord.html");
  })
 
 module.exports = router;
